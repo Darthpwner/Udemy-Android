c37ae904d4b1921da9026f696624a70ae125952f
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-18 22:31:19 -0800

    Need to add conversion rate

---
+++ CurrencyConverter/app/src/main/java/matthewallenlinsoftware/currencyconverter/MainActivity.java
@@ -0,0 +1,47 @@
+package matthewallenlinsoftware.currencyconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public void convert(View view) {
+        EditText dollarField = (EditText) findViewById(R.id.dollarField);
+
+        Log.i("dollarField", dollarField.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
