9f7c37d98c01f6c2d92634ebc0334745e22aa4d7
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-26 14:53:08 -0800

    Remove VideoDemo

--- TimerDemo/app/src/main/java/matthewallenlinsoftware/timerdemo/MainActivity.java
+++ TimerDemo/app/src/main/java/matthewallenlinsoftware/timerdemo/MainActivity.java
@@ -1,7 +1,7 @@
 package matthewallenlinsoftware.timerdemo;
 
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -15,19 +15,33 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Timer code
-        final Handler handler = new Handler();    //Allows chunks of code to be put on in a delayed way
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                //Insert code ot be run every second
-                Log.i("Runnable has run!", "a second must have passed");
+//        //Timer code
+//        final Handler handler = new Handler();    //Allows chunks of code to be put on in a delayed way
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                //Insert code ot be run every second
+//                Log.i("Runnable has run!", "a second must have passed");
+//
+//                handler.postDelayed(this, 1000);
+//            }
+//        };
+//
+//        handler.post(run);
 
-                handler.postDelayed(this, 1000);
+        //Countdown Timer
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisecondsUntilDone) {    //This method will give us a variable to use
+                //Countdown is counting down (every second)
+                Log.i("Seconds left", String.valueOf(millisecondsUntilDone / 1000));    //convert long -> string
             }
-        };
 
-        handler.post(run);
+            public void onFinish() {
+                //Counter is finished! (after 10 seconds)
+                Log.i("Done!", "Countdown Timer Finished");
+                //Countdown object is destroyed when onFinish is called
+            }
+        }.start();
     }
 
     @Override
