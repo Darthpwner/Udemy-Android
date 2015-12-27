6f83ee6c3fe15abb48c12b5552b0149f7b174739
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-26 22:40:51 -0800

    Fix the GrO button

--- BrainTrainer/app/src/main/java/matthewallenlinsoftware/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/matthewallenlinsoftware/braintrainer/MainActivity.java
@@ -1,17 +1,27 @@
 package matthewallenlinsoftware.braintrainer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button startButton;
+
+    public void start(View view) {
+        startButton.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
     }
 
     @Override
