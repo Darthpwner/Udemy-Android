da2c6983b13f3aafeb0d63802021340488bb36fe
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-28 12:57:58 -0800

    Rob made some mistakes in here

--- GuesstheCelebrityMatt/app/src/main/java/matthewallenlinsoftware/guessthecelebritymatt/MainActivity.java
+++ GuesstheCelebrityMatt/app/src/main/java/matthewallenlinsoftware/guessthecelebritymatt/MainActivity.java
@@ -1,12 +1,56 @@
 package matthewallenlinsoftware.guessthecelebritymatt;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
