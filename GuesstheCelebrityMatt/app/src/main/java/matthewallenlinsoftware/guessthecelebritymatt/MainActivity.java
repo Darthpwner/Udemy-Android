6f3df623a9082114ba18b7b5dfade97dc172cf64
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-28 23:15:53 -0800

    Fixed up some Bitmap stuff

--- GuesstheCelebrityMatt/app/src/main/java/matthewallenlinsoftware/guessthecelebritymatt/MainActivity.java
+++ GuesstheCelebrityMatt/app/src/main/java/matthewallenlinsoftware/guessthecelebritymatt/MainActivity.java
@@ -1,5 +1,7 @@
 package matthewallenlinsoftware.guessthecelebritymatt;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -13,10 +15,44 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -64,6 +100,25 @@ public class MainActivity extends ActionBarActivity {
 
         try {
             result = task.execute("http://www.posh24.com/celebrities").get();
+
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while(m.find()) {
+                celebURLs.add(m.group(1));
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while(m.find()) {
+                celebURLs.add(m.group(1));
+            }
+
+            Random random = new Random();
+            chosenCeleb = random.nextInt(celebURLs.size());
 
             Log.i("Contents of URL", result);
         } catch(InterruptedException e) {
