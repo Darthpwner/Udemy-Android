647cf33824d08caf474cf6dab61adb3b238ed429
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-01 14:02:44 -0800

    Map stuff

--- LocationMapDemo/app/src/main/java/matthewallenlinsoftware/locationmapdemo/MapsActivity.java
+++ LocationMapDemo/app/src/main/java/matthewallenlinsoftware/locationmapdemo/MapsActivity.java
@@ -1,28 +1,43 @@
 package matthewallenlinsoftware.locationmapdemo;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity, LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    LocationManager locationManager;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        provider = locationManager.getBestProvider(new Criteria(), false);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
     private void setUpMapIfNeeded() {
@@ -40,5 +55,36 @@ public class MapsActivity extends FragmentActivity {
 
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+
+        Log.i("Latitude", lat.toString());
+       // Log.i("Longitude", lng.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
     }
 }
