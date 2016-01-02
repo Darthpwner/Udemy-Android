fb5851bf02ffc7ae1be7320540427a188a11abf2
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-01 17:18:38 -0800

    Able to parse geolocations

--- LocationMapDemo/app/src/main/java/matthewallenlinsoftware/locationmapdemo/MapsActivity.java
+++ LocationMapDemo/app/src/main/java/matthewallenlinsoftware/locationmapdemo/MapsActivity.java
@@ -1,18 +1,25 @@
 package matthewallenlinsoftware.locationmapdemo;
 
 import android.content.Context;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements LocationListener {
 
@@ -71,6 +78,19 @@ public class MapsActivity extends FragmentActivity implements LocationListener {
         mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Your location"));
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 12));
+
+        //Geocoding
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(lat, lng, 1);
+
+            if(listAddresses != null && listAddresses.size() > 0) {
+                Log.i("PlaceInfo", listAddresses.get(0).toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
