f4f6567325fc1eabbda05d4caf84159227474bd1
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-02 15:23:59 -0800

    Added Rob's SharedPreferencesDemo

---
+++ SharedPreferencesDemo/app/src/main/java/com/example/robpercival/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.robpercival.sharedpreferencesdemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.robpercival.sharedpreferencesdemo", Context.MODE_PRIVATE);
+
+        //sharedPreferences.edit().putString("username", "rob").apply();
+
+        String username = sharedPreferences.getString("username", "");
+
+        Log.i("username", username);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
