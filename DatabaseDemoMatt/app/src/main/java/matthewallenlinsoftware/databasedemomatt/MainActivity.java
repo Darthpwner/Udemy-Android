46ca8150f1758577e4b503baa05db3c3e0017415
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-03 01:01:35 -0800

    Both databases work

--- DatabaseDemoMatt/app/src/main/java/matthewallenlinsoftware/databasedemomatt/MainActivity.java
+++ DatabaseDemoMatt/app/src/main/java/matthewallenlinsoftware/databasedemomatt/MainActivity.java
@@ -46,6 +46,32 @@ public class MainActivity extends Activity {
         } catch(Exception e) {
             e.printStackTrace();
         }
+
+        try {
+            SQLiteDatabase eventsDB = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+
+            eventsDB.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR, year INT(4))");
+
+            eventsDB.execSQL("INSERT INTO events (event, year) VALUES ('End of WW2', 1945)");
+
+            eventsDB.execSQL("INSERT INTO events (event, year) VALUES ('Wham split up', 1986)");
+
+            Cursor c = eventsDB.rawQuery("SELECT * FROM events", null);
+
+            int eventIndex = c.getColumnIndex("event");
+            int yearIndex = c.getColumnIndex("year");
+
+            c.moveToFirst();
+
+            while(c != null) {
+                Log.i("Results - event", c.getString(eventIndex));
+                Log.i("Results - year", Integer.toString(c.getInt(yearIndex)));
+
+                c.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
