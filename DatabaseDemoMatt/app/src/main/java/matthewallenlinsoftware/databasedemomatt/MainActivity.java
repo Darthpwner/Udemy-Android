3a65e078d2839f4dd51f246aa218166cdf27cef0
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-03 00:22:21 -0800

    Database works

---
+++ DatabaseDemoMatt/app/src/main/java/matthewallenlinsoftware/databasedemomatt/MainActivity.java
@@ -0,0 +1,72 @@
+package matthewallenlinsoftware.databasedemomatt;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            //Create database
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);    //Opens database if it exists or creates it
+            //Put a table in your database
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)"); //Rob has single quotes to show it is a string
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Matthew', 21)");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Tommy', 4)");
+
+            //Used to query from database, loops through all the results of a particular query
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            //Deal with column indices in Android
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+            while(c != null) {
+                Log.i("name", c.getString(nameIndex));
+                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+
+                c.moveToNext();
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
