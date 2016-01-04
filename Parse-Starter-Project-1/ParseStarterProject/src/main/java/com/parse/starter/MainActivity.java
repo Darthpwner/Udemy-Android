43999f6d6255d186a0bf756c1c7ec8732df47b7f
Matthew Allen Lin <Darthpwner@s-169-232-69-149.resnet.ucla.edu>
2016-01-04 12:58:21 -0800

    Check to see if login was successful

--- Parse-Starter-Project-1/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Parse-Starter-Project-1/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -10,14 +10,14 @@ package com.parse.starter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 //ActionBarActivity is deprecated
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +26,67 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+      ParseUser user = new ParseUser();
+
+      //Required
+      user.setUsername("Matthew Lin");
+      user.setPassword("myPass");
+
+      //Optional
+      user.setEmail("hello");
+
+      user.put("phone number", 911);
+
+      //Verifies if sign up was successful
+      user.signUpInBackground(new SignUpCallback() {
+          @Override
+          public void done(ParseException e) {
+              if(e == null) {
+                  Log.i("signUp", "Successful");
+              } else {
+                  Log.i("signUp", "Failed");
+                  e.printStackTrace();
+              }
+          }
+      });
+
+//      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//      query.whereEqualTo("username", "Matthew");
+//      query.setLimit(1);
+//
+//      query.findInBackground(new FindCallback<ParseObject>() {
+//          @Override
+//          public void done(List<ParseObject> objects, ParseException e) {
+//              if(e == null) {
+//                  if(objects.size() > 0) {
+//                      objects.get(0).put("score", 69);   //ParseObject so you can do the usual stuff
+//                      objects.get(0).saveInBackground();
+//                  }
+//              }
+//          }
+//      });
+
+//      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//
+//      query.whereEqualTo("username", "Matthew");
+//      query.setLimit(1);    //Make sure we don't return unnecessary users
+//
+//      query.findInBackground(new FindCallback<ParseObject>() {
+//          @Override
+//          public void done(List<ParseObject> objects, ParseException e) {
+//              if(e == null) {
+//                  Log.i("findInBackground", "Retrieved" + objects.size() + " results");
+//
+//                  for(ParseObject object: objects) {    //Loops through objects list, which contains ParseObjects
+//                      Log.i("findInBackgroundUser", String.valueOf(object.get("username")));
+//
+//                  }
+//              }
+//          }
+//      });
+
+
 
       /*
       ParseObject score = new ParseObject("Score"); //We are creating a general class called Score, and we are also creating a particular object of that class called "score"
@@ -50,17 +111,18 @@ public class MainActivity extends AppCompatActivity {
           }
       });   //Tries to save right away but if Internet is not there, it will save it later
 */
-      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
-
-      query.getInBackground("0j5g5c7tda", new GetCallback<ParseObject>() {
-          @Override
-          public void done(ParseObject parseObject, ParseException e) {
-              if(e == null) {
-                  parseObject.put("score", 200);
-                  parseObject.saveInBackground();
-              }
-          }
-      });
+//      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//
+//      query.getInBackground("0j5g5c7tda", new GetCallback<ParseObject>() {
+//          @Override
+//          public void done(ParseObject parseObject, ParseException e) {
+//              if(e == null) {
+//                  parseObject.put("score", 200);
+//                  parseObject.put("username", "Matthew");
+//                  parseObject.saveInBackground();
+//              }
+//          }
+//      });
 
       ParseAnalytics.trackAppOpenedInBackground(getIntent()); //This has to be at the end!
   }
--- Parse-Starter-Project-1/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ Parse-Starter-Project-1/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -12,7 +12,6 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
-import com.parse.ParseUser;
 
 
 public class StarterApplication extends Application {
@@ -27,7 +26,7 @@ public class StarterApplication extends Application {
     // Add your initialization code here
     Parse.initialize(this);
 
-    ParseUser.enableAutomaticUser();
+    //ParseUser.enableAutomaticUser();
     ParseACL defaultACL = new ParseACL();
     // Optionally enable public read access.
     // defaultACL.setPublicReadAccess(true);
